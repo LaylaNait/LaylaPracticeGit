7372c2af9e267cbc88253188f52a18050119df74
Layla <layla.biologie@gmail.com>
2022-02-05 18:32:39 -0500

    second commit

--- src/laylaPractice/Practice1.java
+++ src/laylaPractice/Practice1.java
@@ -3,9 +3,7 @@ package laylaPractice;
 public class Practice1 {
     public static void main(String[] args) {
 
-
-
-
-
+        System.out.println("Work1");
+        System.out.println("Work1 done");
     }
 }
